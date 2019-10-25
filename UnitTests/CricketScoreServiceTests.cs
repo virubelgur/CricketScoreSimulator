b74fe525bf1a18547c525ca1dd2a681b153cf9b2
Veeresh Panchakshari Belgur <veereshpanchaksharibelgur@eurofins.com>
2019-10-25 14:18:05 +0530

    Add new test cases

--- UnitTests/CricketScoreServiceTests.cs
+++ UnitTests/CricketScoreServiceTests.cs
@@ -20,13 +20,17 @@ namespace UnitTests
         [EmbeddedResourceAsJsonDeconstructedArray("UnitTests.MatchConfigurations.json")]
         public void When_GetScoreCard_WithValidConfigurations_ScoreCardReturns(MatchConfiguration configuration)
         {
+            //act
             var result = _domain.GetScoreCard(configuration);
+
+            //asserts
             result.ShouldNotBeNull();
             result.ScorePerOver.Count.ShouldBeLessThanOrEqualTo(configuration.OversLimit);
             result.ScorePerOver.First().Value.First().ScoredBy.ShouldBe(configuration.Players.First().Name);
             result.ScorePerOver.Any(x => x.Value.Sum(y => y.RunScored) > 36).ShouldBeFalse();
             result.ScorePerOver.Any(x => x.Value.Any(y=>y.RunScored > 6 || y.RunScored < -1)).ShouldBeFalse();
             result.ScorePerOver.Sum(x=>x.Value.Sum(y=>y.RunScored)).ShouldBeLessThanOrEqualTo(configuration.RunsToWin + 5);
+            result.ScorePerOver.Keys.Count.ShouldBeLessThanOrEqualTo(configuration.OversLimit);
             if (result.ResultSummary.IsBattingTeamWon)
             {
                 result.ResultSummary.BallsLeft.ShouldNotBeNull();
